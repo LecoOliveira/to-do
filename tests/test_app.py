15208c68b344d62fa46ac2b41fb798364a3f22f3
LecoOliveira <lecoverde10@gmail.com>
2023-08-05 15:31:02 -0300

    Adicionando as rotas CRUD e seus testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,63 @@
-from fastapi.testclient import TestClient
-from to_do.app import app
-
-client = TestClient(app)
-
-
-def test_root_deve_retornar_200_e_hello_world():
+def test_root_deve_retornar_200_e_hello_world(client):
     response = client.get('/')
+
     assert response.status_code == 200
     assert response.json() == {'message': 'Hello world'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_get_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == 200
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'alex',
+            'email': 'teste@teste.com',
+            'password': 'testando',
+        },
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {
+        'username': 'alex',
+        'email': 'teste@teste.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == 200
+    assert response.json() == {'detail': 'User deleted'}
--- to_do/app.py
+++ to_do/app.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from to_do.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
@@ -6,3 +8,41 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'message': 'Hello world'}
+
+
+database = []
+
+
+@app.post('/users/', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    del database[user_id - 1]
+
+    return {'detail': 'User deleted'}
